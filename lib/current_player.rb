fa68d2eb12c62b1f7a5209daefa5f8f557194e5c
Mohammed Hamood <m_alkhalifah88@yahoo.com>
2016-10-12 07:33:14 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  i = 0
+  count = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      count +=1
+    end
+end
+count
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
